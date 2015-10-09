3734ea24e48c574fd66f830e4d136a5bacd3dfff
Navid Emami <emami.navid@hotmail.com>
2015-10-09 22:36:19 +0330

    PHPDoc implemented with some minor fixes in code.

--- jalaaliTest.php
+++ jalaaliTest.php
@@ -6,39 +6,39 @@ class JalaaliTest extends PHPUnit_Framework_Testcase
 {
     public function testToJalaali()
     {
-        $this->assertEquals(Jalaali::toJalaali(1989, 01, 28), array('jy' => 1367, 'jm' => 11, 'jd' => 08));
+        $this->assertEquals(toJalaali(1989, 01, 28), array('jy' => 1367, 'jm' => 11, 'jd' => 08));
     }
     
     public function testToGregorian()
     {
-        $this->assertEquals(Jalaali::toGregorian(1367, 11, 08), array('gy' => 1989, 'gm' => 01, 'gd' => 28));
+        $this->assertEquals(toGregorian(1367, 11, 08), array('gy' => 1989, 'gm' => 01, 'gd' => 28));
     }
     
     public function testIsValidJalaaliDate()
     {
-        $this->assertFalse(Jalaali::isValidJalaaliDate(-62, 12, 29));
-        $this->assertFalse(Jalaali::isValidJalaaliDate(3178, 01, 01));
-        $this->assertTrue(Jalaali::isValidJalaaliDate(-61, 01, 01));
-        $this->assertTrue(Jalaali::isValidJalaaliDate(3177, 12, 29));
+        $this->assertFalse(isValidJalaaliDate(-62, 12, 29));
+        $this->assertFalse(isValidJalaaliDate(3178, 01, 01));
+        $this->assertTrue(isValidJalaaliDate(-61, 01, 01));
+        $this->assertTrue(isValidJalaaliDate(3177, 12, 29));
     }
     
     public function testIsLeapJalaaliYear()
     {
-        $this->assertFalse(Jalaali::isLeapJalaaliYear(1393));
-        $this->assertFalse(Jalaali::isLeapJalaaliYear(1394));
-        $this->assertTrue(Jalaali::isLeapJalaaliYear(1395));
-        $this->assertFalse(Jalaali::isLeapJalaaliYear(1396));
+        $this->assertFalse(isLeapJalaaliYear(1393));
+        $this->assertFalse(isLeapJalaaliYear(1394));
+        $this->assertTrue(isLeapJalaaliYear(1395));
+        $this->assertFalse(isLeapJalaaliYear(1396));
     }
     
     public function testJalaaliMonthLength()
     {
-        $this->assertEquals(Jalaali::jalaaliMonthLength(1393, 01), 31);
-        $this->assertEquals(Jalaali::jalaaliMonthLength(1393, 03), 31);
-        $this->assertEquals(Jalaali::jalaaliMonthLength(1393, 06), 31);
-        $this->assertEquals(Jalaali::jalaaliMonthLength(1393, 08), 30);
-        $this->assertEquals(Jalaali::jalaaliMonthLength(1393, 10), 30);
-        $this->assertEquals(Jalaali::jalaaliMonthLength(1393, 12), 29);
-        $this->assertEquals(Jalaali::jalaaliMonthLength(1394, 12), 29);
-        $this->assertEquals(Jalaali::jalaaliMonthLength(1395, 12), 30);
+        $this->assertEquals(jalaaliMonthLength(1393, 01), 31);
+        $this->assertEquals(jalaaliMonthLength(1393, 03), 31);
+        $this->assertEquals(jalaaliMonthLength(1393, 06), 31);
+        $this->assertEquals(jalaaliMonthLength(1393, 08), 30);
+        $this->assertEquals(jalaaliMonthLength(1393, 10), 30);
+        $this->assertEquals(jalaaliMonthLength(1393, 12), 29);
+        $this->assertEquals(jalaaliMonthLength(1394, 12), 29);
+        $this->assertEquals(jalaaliMonthLength(1395, 12), 30);
     }
 }
